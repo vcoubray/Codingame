0e6dc4a24d13845c482fc90f7be834205479c9ed
Vincent <vincentcoubray@gmail.com>
2021-02-13 19:45:15 +0100

    Code4Life - Wood league 2 passed

--- Contests/Code4Life/src/main/kotlin/fr/vco/codingame/contest/code4life/Main.kt
+++ Contests/Code4Life/src/main/kotlin/fr/vco/codingame/contest/code4life/Main.kt
@@ -1,13 +1,34 @@
 package fr.vco.codingame.contest.code4life
 
 import java.util.*
-import java.io.*
-import java.math.*
 
-/**
- * Bring data on patient samples from the diagnosis machine to the laboratory with enough molecules to produce medicine!
- **/
-fun main(args : Array<String>) {
+fun log(message: String) = System.err.println(message)
+
+enum class Module { DIAGNOSIS, MOLECULES, LABORATORY, START_POS }
+val moleculeType = listOf("A","B","C","D","E")
+
+fun Int.toMoleculeType() = moleculeType[this]
+
+class Bot(
+    val module: Module,
+    val score: Int,
+    val storage: List<Int>
+) {
+    fun hasEnough(cost : List<Int>) : Boolean{
+       return storage.zip(cost).all{(a,b) -> a >= b}
+    }
+}
+
+data class Sample(
+    val id: Int,
+    val owner: Int,
+    val health: Int,
+    val costs: List<Int>
+){
+    fun rentability()= health.toFloat()/ costs.sum()
+}
+
+fun main() {
     val input = Scanner(System.`in`)
     val projectCount = input.nextInt()
     for (i in 0 until projectCount) {
@@ -20,8 +41,9 @@ fun main(args : Array<String>) {
 
     // game loop
     while (true) {
-        for (i in 0 until 2) {
-            val target = input.next()
+
+        val bots = List(2) {
+            val module = input.next()
             val eta = input.nextInt()
             val score = input.nextInt()
             val storageA = input.nextInt()
@@ -34,14 +56,21 @@ fun main(args : Array<String>) {
             val expertiseC = input.nextInt()
             val expertiseD = input.nextInt()
             val expertiseE = input.nextInt()
+            Bot(
+                Module.valueOf(module),
+                score,
+                listOf(storageA, storageB, storageC, storageD, storageE)
+            )
         }
+
         val availableA = input.nextInt()
         val availableB = input.nextInt()
         val availableC = input.nextInt()
         val availableD = input.nextInt()
         val availableE = input.nextInt()
         val sampleCount = input.nextInt()
-        for (i in 0 until sampleCount) {
+
+        val samples = List(sampleCount) {
             val sampleId = input.nextInt()
             val carriedBy = input.nextInt()
             val rank = input.nextInt()
@@ -52,10 +81,29 @@ fun main(args : Array<String>) {
             val costC = input.nextInt()
             val costD = input.nextInt()
             val costE = input.nextInt()
+            Sample(
+                sampleId,
+                carriedBy,
+                health,
+                listOf(costA,costB, costC, costD, costE)
+            )
         }
 
 
+        val me = bots.first()
+        val mySample = samples.firstOrNull { it.owner == 0 }
+        val availableSamples = samples.filter{it.owner == -1}.sortedByDescending { it.rentability() }
 
-        println("GOTO DIAGNOSIS")
+        log(mySample?.toString()?:"")
+        val action = when {
+            mySample == null && me.module != Module.DIAGNOSIS -> "GOTO ${Module.DIAGNOSIS}"
+            mySample == null -> "CONNECT ${availableSamples.first().id}"
+            me.hasEnough(mySample.costs) && me.module !=Module.LABORATORY -> "GOTO ${Module.LABORATORY}"
+            me.hasEnough(mySample.costs)-> "CONNECT ${mySample.id}"
+            me.module != Module.MOLECULES -> "GOTO ${Module.MOLECULES}"
+            else -> "CONNECT ${me.storage.zip(mySample.costs).indexOfFirst{(a,b) -> a < b}.toMoleculeType()}"
+        }
+
+        println(action)
     }
 }
