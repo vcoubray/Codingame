2a56e77790cd6da24a1c8f04babc69fb2a777676
Vincent <vincentcoubray@gmail.com>
2021-02-13 18:47:25 +0100

    Code4Life - Initialization

---
+++ Contests/Code4Life/README.md
@@ -0,0 +1,9 @@
+# Code 4 Life
+
+Statements : [Code 4 Life](https://www.codingame.com/multiplayer/bot-programming/code4life)
+
+# Usage 
+
+```bash
+./gradlew :Contests:Code4Life:codingame
+```
---
+++ Contests/Code4Life/src/main/kotlin/fr/vco/codingame/contest/code4life/Main.kt
@@ -0,0 +1,61 @@
+package fr.vco.codingame.contest.code4life
+
+import java.util.*
+import java.io.*
+import java.math.*
+
+/**
+ * Bring data on patient samples from the diagnosis machine to the laboratory with enough molecules to produce medicine!
+ **/
+fun main(args : Array<String>) {
+    val input = Scanner(System.`in`)
+    val projectCount = input.nextInt()
+    for (i in 0 until projectCount) {
+        val a = input.nextInt()
+        val b = input.nextInt()
+        val c = input.nextInt()
+        val d = input.nextInt()
+        val e = input.nextInt()
+    }
+
+    // game loop
+    while (true) {
+        for (i in 0 until 2) {
+            val target = input.next()
+            val eta = input.nextInt()
+            val score = input.nextInt()
+            val storageA = input.nextInt()
+            val storageB = input.nextInt()
+            val storageC = input.nextInt()
+            val storageD = input.nextInt()
+            val storageE = input.nextInt()
+            val expertiseA = input.nextInt()
+            val expertiseB = input.nextInt()
+            val expertiseC = input.nextInt()
+            val expertiseD = input.nextInt()
+            val expertiseE = input.nextInt()
+        }
+        val availableA = input.nextInt()
+        val availableB = input.nextInt()
+        val availableC = input.nextInt()
+        val availableD = input.nextInt()
+        val availableE = input.nextInt()
+        val sampleCount = input.nextInt()
+        for (i in 0 until sampleCount) {
+            val sampleId = input.nextInt()
+            val carriedBy = input.nextInt()
+            val rank = input.nextInt()
+            val expertiseGain = input.next()
+            val health = input.nextInt()
+            val costA = input.nextInt()
+            val costB = input.nextInt()
+            val costC = input.nextInt()
+            val costD = input.nextInt()
+            val costE = input.nextInt()
+        }
+
+
+
+        println("GOTO DIAGNOSIS")
+    }
+}
