f07a873dee9f0e1a33fedfd54973a0743e649aee
Vincent <vincentcoubray@gmail.com>
2022-04-21 23:46:48 +0200

    Contests Spring Challenge 2022 - pass wood leagues

--- Contests/Spring-Challenge-2022/src/main/kotlin/fr/vco/codingame/contest/springchallenge2022/Main.kt
+++ Contests/Spring-Challenge-2022/src/main/kotlin/fr/vco/codingame/contest/springchallenge2022/Main.kt
@@ -1,14 +1,62 @@
 package fr.vco.codingame.contest.springchallenge2022
 
 import java.util.*
-import java.io.*
-import java.math.*
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
-fun main(args : Array<String>) {
+const val MONSTER = 0
+const val MY_HERO = 1
+const val OPP_HERO = 2
+
+fun Int.square() = this * this
+
+data class Entity(
+    val id: Int,
+    val type: Int,
+    val x: Int,
+    val y: Int,
+    val shieldLife: Int,
+    val isControlled: Int,
+    val health: Int,
+    val vx: Int,
+    val vy: Int,
+    val nearBase: Int,
+    val threatFor: Int,
+    var targeted: Boolean = false,
+    var target: Entity? = null,
+) {
+    fun toHero() = Hero(id, type == MY_HERO, x, y)
+    fun toMonster() = Monster(id, x, y, health, vx, vy, nearBase, threatFor)
+
+    fun dist(entity: Entity) = dist(entity.x, entity.y)
+    fun dist(x: Int, y: Int) = (this.x - x).square() + (this.y - y).square()
+}
+
+data class Hero(
+    val id: Int,
+    val isGoodGuy: Boolean,
+    val x: Int,
+    val y: Int,
+    var target: Entity? = null
+) {
+
+    fun play() {
+        val action = target?.let { "MOVE ${it.x} ${it.y}" } ?: "WAIT"
+        println(action)
+    }
+}
+
+data class Monster(
+    val id: Int,
+    val x: Int,
+    val y: Int,
+    val health: Int,
+    val vx: Int,
+    val vy: Int,
+    val nearBase: Int,
+    val threatFor: Int,
+)
+
+
+fun main() {
     val input = Scanner(System.`in`)
     val baseX = input.nextInt() // The corner of the map representing your base
     val baseY = input.nextInt()
@@ -21,27 +69,38 @@ fun main(args : Array<String>) {
             val mana = input.nextInt() // Ignore in the first league; Spend ten mana to cast a spell
         }
         val entityCount = input.nextInt() // Amount of heros and monsters you can see
-        for (i in 0 until entityCount) {
-            val id = input.nextInt() // Unique identifier
-            val type = input.nextInt() // 0=monster, 1=your hero, 2=opponent hero
-            val x = input.nextInt() // Position of this entity
-            val y = input.nextInt()
-            val shieldLife = input.nextInt() // Ignore for this league; Count down until shield spell fades
-            val isControlled = input.nextInt() // Ignore for this league; Equals 1 when this entity is under a control spell
-            val health = input.nextInt() // Remaining health of this monster
-            val vx = input.nextInt() // Trajectory of this monster
-            val vy = input.nextInt()
-            val nearBase = input.nextInt() // 0=monster with no target yet, 1=monster targeting a base
-            val threatFor = input.nextInt() // Given this monster's trajectory, is it a threat to 1=your base, 2=your opponent's base, 0=neither
+        val entities = List(entityCount) {
+            Entity(
+                id = input.nextInt(),
+                type = input.nextInt(), // 0=monster, 1=your hero, 2=opponent hero
+                x = input.nextInt(),
+                y = input.nextInt(),
+                shieldLife = input.nextInt(),
+                isControlled = input.nextInt(),
+                health = input.nextInt(),
+                vx = input.nextInt(),
+                vy = input.nextInt(),
+                nearBase = input.nextInt(), // 0=monster with no target yet, 1=monster targeting a base
+                threatFor = input.nextInt(), // Given this monster's trajectory, is it a threat to 1=your base, 2=your opponent's base, 0=neither
+            )
         }
-        for (i in 0 until heroesPerPlayer) {
 
-            // Write an action using println()
-            // To debug: System.err.println("Debug messages...");
+        val myHeroes = entities.filter { it.type == MY_HERO }.sortedBy { it.id }.map{it.toHero()}
+        val monsters = entities.filter { it.type == MONSTER }
 
+        monsters.filterNot { it.threatFor == 2 }
+            .filterNot { it.targeted }
+            .sortedWith(compareBy({ -it.threatFor }, { it.dist(baseX, baseY) }))
+            .take(myHeroes.size)
+            .forEach { monster ->
+                myHeroes
+                    .filter { it.target == null }
+                    .minByOrNull { monster.dist(it.x, it.y) }
+                    ?.target = monster.apply { this.targeted = true }
+            }
 
-            // In the first league: MOVE <x> <y> | WAIT; In later leagues: | SPELL <spellParams>;
-            println("WAIT")
-        }
+        myHeroes.forEach { it.play() }
+
     }
 }
+
