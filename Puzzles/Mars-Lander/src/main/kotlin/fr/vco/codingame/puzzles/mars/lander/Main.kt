f842f65cb8e822e223c8f557b5d0e95ab822c6c4
Vincent <vincentcoubray@gmail.com>
2022-07-09 23:55:24 +0200

    Puzzle Mars lander - Check crossing

--- Puzzles/Mars-Lander/src/main/kotlin/fr/vco/codingame/puzzles/mars/lander/Main.kt
+++ Puzzles/Mars-Lander/src/main/kotlin/fr/vco/codingame/puzzles/mars/lander/Main.kt
@@ -5,6 +5,15 @@ import kotlin.math.*
 
 const val MARS_GRAVITY = 3.711
 
+
+data class Action(
+    val rotate: Int,
+    val power: Int
+)
+
+
+fun minMax(value: Int, min: Int, max: Int) = max(min(value, max), min)
+
 data class State(
     var x: Double,
     var y: Double,
@@ -24,10 +33,10 @@ data class State(
         input.nextInt()
     )
 
-    fun play(rotate: Int, power: Int) {
+    fun play(action: Action) {
 
-        this.power = power
-        this.rotate = rotate
+        this.power =  minMax(power + action.power, 0, 4)
+        this.rotate = minMax(rotate + action.rotate + (-15..15).random(), -90, 90)
 
         val rad = Math.toRadians(this.rotate.toDouble())
         val newXSpeed = (this.xSpeed - this.power * sin(rad))
@@ -42,21 +51,50 @@ data class State(
         this.fuel -= power
     }
 
+
+    fun play(actions: List<Action>, surface: Surface) {
+        val lastX = x
+        val lastY = y
+        for (action in actions) {
+            play(action)
+            if(surface.cross(Segment(lastX to lastY, x to y ))) {
+                System.err.println("crossing")
+                break
+            }
+        }
+    }
+
 }
+
+data class Segment(val start: Pair<Double, Double>, val end: Pair<Double, Double>)
 
 data class Surface(
     val segments: List<Segment>
 ) {
     constructor(input: Scanner) : this(
-        List(input.nextInt()) { input.nextInt() to input.nextInt() }
+        List(input.nextInt()) { input.nextDouble() to input.nextDouble() }
             .windowed(2)
             .map { (a, b) -> Segment(a, b) }
     )
+
+    private fun cross(s1: Segment, s2: Segment): Boolean {
+        val s1x = s1.end.first - s1.start.first
+        val s1y = s1.end.second - s1.start.second
+        val s2x = s2.end.first - s2.start.first
+        val s2y = s2.end.second - s2.start.second
+
+        val s =
+            (-s1y * (s1.start.first - s2.start.first) + s1x * (s1.start.second - s2.start.second)) / (-s2x * s1y + s1x * s2y)
+        val t =
+            (-s2y * (s1.start.first - s2.start.first) + s2x * (s1.start.second - s2.start.second)) / (-s2x * s1y + s1x * s2y)
+
+        return (s in 0.0..1.0 && t in 0.0.. 1.0)
+    }
+
+    fun cross(segment: Segment) :Boolean {
+        return segments.any{cross(it, segment)}
+    }
 }
-
-data class Segment(val start: Pair<Int, Int>, val end: Pair<Int, Int>)
-
-fun minMax(value: Int, min: Int, max: Int) = max(min(value, max), min)
 
 fun main() {
     val input = Scanner(System.`in`)
@@ -67,11 +105,11 @@ fun main() {
     // game loop
     while (true) {
 
+        val power =  (-1..1).random()
+        val rotate = (-15..15).random()
 
-        val power = minMax(realState.power + (-1..1).random(), 0, 4)
-        val rotate = minMax(realState.rotate + (-15..15).random(), -90, 90)
-
-        realState.play(rotate, power)
+        val action = Action(rotate, power)
+        realState.play(listOf(action),surface)
         System.err.println(realState)
         println("$rotate $power")
 
