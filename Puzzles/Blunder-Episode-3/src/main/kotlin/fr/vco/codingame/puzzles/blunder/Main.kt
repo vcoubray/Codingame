b053ff6037a4c5049a7103630535e5f717ef888d
Vincent <vincentcoubray@gmail.com>
2022-09-15 19:24:39 +0200

    Blunder Episode 3 - Fix for test O(1)

--- Puzzles/Blunder-Episode-3/src/main/kotlin/fr/vco/codingame/puzzles/blunder/Main.kt
+++ Puzzles/Blunder-Episode-3/src/main/kotlin/fr/vco/codingame/puzzles/blunder/Main.kt
@@ -15,7 +15,6 @@ val MODELS = mapOf<String, (Double) -> Double>(
     "O(2^n)" to { n -> 2.0.pow(n) }
 )
 
-
 fun main() {
     val input = Scanner(System.`in`)
     val N = input.nextInt()
@@ -24,19 +23,20 @@ fun main() {
         val (num, t) = List(2) { input.nextDouble() }
         num to t
     }
+    val mean = sample.map { (_, t) -> t }.average()
 
     val (n0, t0) = sample.first()
     val (nf, tf) = sample.last()
 
     MODELS
         .map { (id, function) ->
-            val vComplexity = (function(nf) - function(n0)).takeIf { it != 0.0 } ?: 1.0
-            val ratio = (tf - t0) / vComplexity
-            val offset = t0 - (function(n0) * ratio)
+            val slope = (function(nf) - function(n0))
+            val ratio = if (slope != 0.0) (tf - t0) / slope else mean
+            val offset = if (slope != 0.0) t0 - (function(n0) * ratio) else 0.0
+
             id to sample.map { (n, t) -> t - (ratio * function(n) + offset) }.map { it * it }
         }
         .map { (id, residualSquares) -> id to residualSquares.sum() }
-        .onEach(System.err::println)
         .minByOrNull { (_, squareSum) -> squareSum }
         ?.let { (id, _) -> println(id) }
 }
