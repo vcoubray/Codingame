5858d2f81ee44e8cb277bcc4383442856634f277
Vincent <vincentcoubray@gmail.com>
2022-12-12 23:15:31 +0100

    Fall Challenge 2022 - Stock tiles in grid instead of list

--- Contests/Fall-Challenge-2022/src/main/kotlin/fr/vco/codingame/constest/fallchallenge2022/Main.kt
+++ Contests/Fall-Challenge-2022/src/main/kotlin/fr/vco/codingame/constest/fallchallenge2022/Main.kt
@@ -14,15 +14,16 @@ fun main() {
     while (true) {
         var myMatter = input.nextInt()
         val oppMatter = input.nextInt()
-        board.updateTiles(input)
+        board.updateBoard(input)
 
-        val myRobots = board.tiles.filter { it.owner == Owner.ME && it.units > 0 }
+        val tiles = board.grid.flatten()
+        val myRobots = tiles.filter { it.owner == Owner.ME && it.units > 0 }
 
 
         val actions = mutableListOf<Action>()
         // Moves
         actions.addAll(myRobots.mapNotNull { origin ->
-            board.findNearestTargetTile(origin)?.let { target -> Action.Move(origin.units, origin, target) }
+            board.findNearestTargetTile(origin)?.let { target -> Action.Move(origin.units, origin.pos, target.pos) }
         })
 
 //        // Builds
@@ -32,7 +33,9 @@ fun main() {
 //        }
 
         //Spawns
-        actions.addAll(List(myMatter / 10) { board.tiles.filter { it.canSpawn }.random().let { Action.Spawn(1, it) } })
+        actions.addAll(List(myMatter / 10) {
+            tiles.filter { it.canSpawn }.random().let { Action.Spawn(1, it.pos) }
+        })
 
         println(actions.joinToString(";"))
     }
@@ -40,24 +43,24 @@ fun main() {
 
 sealed interface Action {
 
-    class Move(val n: Int, val source: Tile, val target: Tile) : Action {
-        override fun toString() = "MOVE $n ${source.x} ${source.y} ${target.x} ${target.y}"
+    class Move(val n: Int, val source: Position, val target: Position) : Action {
+        override fun toString() = "MOVE $n $source $target"
     }
 
-    class Spawn(val n: Int, val target: Tile) : Action {
-        override fun toString() = "SPAWN $n ${target.x} ${target.y}"
+    class Spawn(val n: Int, val target: Position) : Action {
+        override fun toString() = "SPAWN $n $target"
     }
 
-    class Build(val target: Tile) : Action {
-        override fun toString() = "BUILD ${target.x} ${target.y}"
+    class Build(val target: Position) : Action {
+        override fun toString() = "BUILD $target}"
     }
 
     object Wait : Action {
         override fun toString() = "WAIT"
     }
 
-    class Message : Action {
-        override fun toString() = "MESSAGE "
+    class Message(val message: String) : Action {
+        override fun toString() = "MESSAGE $message"
     }
 }
 
@@ -75,55 +78,45 @@ enum class Owner {
 }
 
 class Tile(
-    val x: Int,
-    val y: Int,
-    val scrapAmount: Int,
-    val owner: Owner,
-    val units: Int,
-    val recycler: Boolean,
-    val canBuild: Boolean,
-    val canSpawn: Boolean,
-    val inRangeOfRecycler: Boolean
+    val pos: Position,
+    var scrapAmount: Int = 0,
+    var owner: Owner = Owner.NEUTRAL,
+    var units: Int = 0,
+    var recycler: Boolean = false,
+    var canBuild: Boolean = false,
+    var canSpawn: Boolean = false,
+    var inRangeOfRecycler: Boolean = false,
 ) {
-    fun dist(tile: Tile) = (x - tile.x).absoluteValue + (y - tile.y).absoluteValue
-
+    fun dist(tile: Tile) = pos.dist(tile.pos)
 }
 
+data class Position(val x: Int, val y: Int) {
+    fun dist(pos: Position) = (x - pos.x).absoluteValue + (y - pos.y).absoluteValue
+    override fun toString() = "$x $y"
+}
 
 class Board(val height: Int, val width: Int) {
 
-    lateinit var tiles: MutableList<Tile>
+    val grid = List(height) { y -> List(width) { x -> Tile(Position(x,y)) } }
 
-    fun updateTiles(input: Scanner) {
-        tiles = mutableListOf()
+    fun updateBoard(input: Scanner) {
         repeat(height) { y ->
             repeat(width) { x ->
-                val scrapAmount = input.nextInt()
-                val owner = input.nextInt()
-                val units = input.nextInt()
-                val recycler = input.nextInt()
-                val canBuild = input.nextInt()
-                val canSpawn = input.nextInt()
-                val inRangeOfRecycler = input.nextInt()
-
-                val tile = Tile(
-                    x,
-                    y,
-                    scrapAmount,
-                    Owner.fromInt(owner),
-                    units,
-                    recycler == 1,
-                    canBuild == 1,
-                    canSpawn == 1,
-                    inRangeOfRecycler == 1
-                )
-                this.tiles.add(tile)
+                grid[y][x].apply {
+                    scrapAmount = input.nextInt()
+                    owner = Owner.fromInt(input.nextInt())
+                    units = input.nextInt()
+                    recycler = input.nextInt() == 1
+                    canBuild = input.nextInt()  == 1
+                    canSpawn = input.nextInt() == 1
+                    inRangeOfRecycler = input.nextInt()  == 1
+                }
             }
         }
     }
 
     fun findNearestTargetTile(origin: Tile): Tile? {
-        return tiles.filter { it.owner != Owner.ME && it.scrapAmount > 0 }.minByOrNull(origin::dist)
+        return grid.flatten().filter { it.owner != Owner.ME && it.scrapAmount > 0 }.minByOrNull(origin::dist)
     }
 
 }
