39d76cc0c483c7ec096ef2db0270130a8df064a9
vince <vincentcoubray@gmail.com>
2025-03-31 22:08:37 +0200

    Vox Codei 2 - Resolve almost all puzzle

--- Puzzles/VoxCodei-Episode2/src/main/kotlin/fr/vco/codingame/puzzles/voxcodei/episode2/Main.kt
+++ Puzzles/VoxCodei-Episode2/src/main/kotlin/fr/vco/codingame/puzzles/voxcodei/episode2/Main.kt
@@ -1,14 +1,25 @@
 package fr.vco.codingame.puzzles.voxcodei.episode2
 
+import kotlin.math.pow
+import kotlin.math.roundToLong
+
+val NODE_INDEX_MASKS = List(64) {
+    2.0.pow(it).roundToLong()
+}
+
+fun initNodeArray(size: Int) = 2.0.pow(size).roundToLong() - 1
+fun Long.remove(index: Int) = if(this[index]) this - NODE_INDEX_MASKS[index] else this
+operator fun Long.get(i: Int): Boolean = (this.shr(i) and 1L) == 1L
+
 const val BOMB_RANGE = 3
-const val EMPTY = -1
-const val WALL = -2
+
 
 data class Position(val x: Int, val y: Int) {
     operator fun plus(other: Position) = Position(x + other.x, y + other.y)
     operator fun minus(other: Position) = Position(x - other.x, y - other.y)
     operator fun times(times: Int) = Position(x * times, y * times)
     operator fun unaryMinus() = Position(-x, -y)
+    override fun toString() = "$x $y"
 }
 
 class GameSimulator(
@@ -17,92 +28,228 @@ class GameSimulator(
     val bombs: Int,
     val turns: Int,
     val nodeCount: Int,
-    val grids: List<List<Int>>,
     val ranges: List<List<Int>>,
+    val rounds: List<Round>
 ) {
 
     fun getFirstState() = State(
         turns,
         bombs,
-        List(nodeCount){true}
+        initNodeArray(nodeCount),
+        initNodeArray(nodeCount)
     )
 
+    fun indexToPosition(index: Int) = Position(index % width, index / width)
 }
+
+class Round(
+    val nodes: List<Int>, // Position of all the nodes for a rounds
+    val nodesInRange: Map<Int, List<Int>> // Associate a cell index with the nodes in range
+)
+
+
 
 class State(
     val turn: Int,
-    val remainingBombs: Int,
-    val nodes: List<Boolean>,
+    val bombCount: Int,
+    val nodes: Long,
+    val notTargetedNodes: Long,
     val bomb1: Int = -1,
     val bomb2: Int = -1,
     val bomb3: Int = -1,
+    val actions: List<Int> = emptyList()
 ) {
 
+    companion object {
+        val FREE_CELLS = MutableList(20*20) { 0 }
+        var FREE_CELLS_COUNTER = 0
+    }
+
+
+    val hash = computeHashCode()
 
     fun isFinal(): Boolean {
-        return turn >= 3 ||
-            nodes.all { false } ||
-            (remainingBombs == 0 && bomb1 == -1 && bomb2 == -1 && bomb3 == 1)
+        return turn <= 3 ||
+                notTargetedNodes == 0L ||
+                bombCount == 0
     }
 
-    fun isWin(): Boolean = nodes.all { false }
+    fun isWin(): Boolean = notTargetedNodes == 0L
 
-    fun getActions(simulator: GameSimulator): List<Int> {
-        val grid = simulator.grids[turn-3]
-        return grid.mapIndexedNotNull { i, cell -> i.takeIf{isFreeCell(cell)} } + -1 
-    }
 
-    private fun isFreeCell(cell: Int) = (cell == EMPTY || (cell >= 0 && !nodes[cell]))
-    
-    
-    private fun play(coord: Int, simulator: GameSimulator) : State {
-        
-        return State(
-            turn -1,
-                remainingBombs-1,
-            explodeBombs(simulator),
-            bomb2,
-            bomb3,
-            coord
-        )
-        
-    }
-    
-    private fun explodeBombs(simulator: GameSimulator): List<Boolean> {
-        val newNodes = nodes.toMutableList()
-        if (bomb1 != -1) {
-            simulator.ranges[bomb1].forEach {
-                if (simulator.grids[turn][it] >= 0) {
-                    newNodes[it] = false
+
+    fun getPossibleActions(simulator: GameSimulator): List<Int> {
+        // System.err.println("turn: $turn")
+        // System.err.println("bombCount: $bombCount")
+        // System.err.println("bomb1: $bomb1")
+        // System.err.println("bomb2: $bomb2")
+        // System.err.println("bomb3: $bomb3")
+        // System.err.println("nodes: ${nodes.toString(2)}")
+        // System.err.println("notTargetedNodes: ${notTargetedNodes.toString(2)}")
+
+        if (bombCount == 0) return listOf(-1)
+        FREE_CELLS_COUNTER++
+        simulator.rounds[turn].nodes.forEach { FREE_CELLS[it] = FREE_CELLS_COUNTER }
+        simulator.ranges.getOrNull(bomb1)?.forEach { FREE_CELLS[it] = FREE_CELLS_COUNTER }
+        simulator.ranges.getOrNull(bomb2)?.forEach { FREE_CELLS[it] = FREE_CELLS_COUNTER }
+        simulator.ranges.getOrNull(bomb3)?.forEach { FREE_CELLS[it] = FREE_CELLS_COUNTER }
+
+        // System.err.println(freeCells.indices.filter{!freeCells[it]})
+        val targets = simulator.rounds[turn - 3].nodesInRange
+            .filter { (cell, _) -> FREE_CELLS[cell] < FREE_CELLS_COUNTER }
+            .mapNotNull { (cell, nodes) ->
+                val targetCount = nodes.count { notTargetedNodes[it] }
+                if (targetCount > 0) {
+                    cell to targetCount
+                } else {
+                    null
                 }
             }
+
+        // simulator.rounds[turn - 3].nodesInRange
+        //     .mapIndexedNotNull{i, nodes -> if(nodes.isNotEmpty()) i to nodes else null}
+        //     .filterIndexed { i, _ -> freeCells[i] }
+        //     .forEach{(i, nodes) ->
+        //     System.err.println("${simulator.indexToPosition(i)} -> ${nodes}")
+        //     }
+
+        // System.err.println("targets: $targets")
+        return targets.sortedByDescending { (_, targetCount) -> targetCount }.map { it.first } + -1
+    }
+
+    fun play(action: Int, simulator: GameSimulator): State {
+
+        return State(
+            turn - 1,
+            if (action != -1) bombCount - 1 else bombCount,
+            removeNode(nodes, simulator.rounds[turn].nodesInRange[bomb1]),
+            removeNode(notTargetedNodes, simulator.rounds[turn - 3].nodesInRange[action]),
+            bomb2,
+            bomb3,
+            action,
+            actions + action
+        )
+    }
+
+    private fun removeNode(nodes: Long, nodeToRemove: List<Int>?): Long {
+        var newNodes = nodes
+        nodeToRemove?.forEach {
+            newNodes = newNodes.remove(it)
         }
         return newNodes
+    }
+
+
+    override fun equals(other: Any?): Boolean {
+        if (other == null) return false
+        if (other !is State) return false
+        return bombCount == other.bombCount &&
+                notTargetedNodes == other.notTargetedNodes
+    }
+
+    private fun computeHashCode(): Int {
+        var hash = 7
+        hash = 31 * hash + ((bomb1 + 1) + (bomb2 + 1) * 401 + (bomb3 + 1) * 401 * 401)
+        hash = 31 * hash + turn
+        hash = 31 * hash + notTargetedNodes.hashCode()
+        return hash
+    }
+
+    override fun hashCode(): Int {
+        return hash
     }
 }
 
 
 fun main() {
+
     val (width, height) = readln().split(" ").map { it.toInt() }
 
     val gridBuilder = SimulatorBuilder(width, height)
     val simulator = gridBuilder.buildSimulator()
     System.err.println("Simulator is ready")
 
-    System.err.println(simulator.turns)
-    val state = simulator.getFirstState()
-    
-    
-    System.err.println(state.getActions(simulator))
-    
-    
-    var i = 0
-    while (true) {
-        println("$i 1")
-        i++
-        val (rounds, bombs) = readln().split(" ").map { it.toInt() }
-        val grid = List(height) { readln() }
-        grid.forEach(System.err::println)
+    println("WAIT")
+
+
+//    var state = simulator.getFirstState().play(-1, simulator)
+//    repeat(9) {
+//        val actions = state.getPossibleActions(simulator)
+//        System.err.println("actions : ${actions}")
+//        System.err.println(state.nodes.toString(2))
+//        System.err.println(state.notTargetedNodes.toString(2))
+//        state = state.play(actions.first(), simulator)
+//
+//    }
+//
+//    System.err.println(state.nodes.toString(2))
+//    System.err.println(state.notTargetedNodes.toString(2))
+
+//    val actions = List(9 ){
+//        val action = state.getPossibleActions(simulator).first()
+//        state = state.play(action, simulator)
+//        action
+//    }
+
+
+
+//    System.err.println(simulator.rounds.joinToString(", ") { it.nodes.joinToString() })
+    val actions = findValidActions(simulator)
+    System.err.println(actions)
+
+    actions.drop(1).map {
+        if (it == -1) "WAIT"
+        else simulator.indexToPosition(it).toString()
+    }.forEach { action ->
+
+        readln().split(" ").map { it.toInt() }
+        List(height) { readln() }.joinToString("")
+        println(action)
     }
 
+    while (true) {
+        val (turns, _) = readln().split(" ").map { it.toInt() }
+        val grid = List(height) { readln() }.joinToString("")
+        val nodes = grid.indices.filter { grid[it] == NODE }.sorted()
+        System.err.println("INPUT :   $turns -> $nodes")
+        System.err.println("COMPUTED: $turns -> ${simulator.rounds[turns].nodes.sorted()}")
+        System.err.println("Ranges :  $turns -> ${simulator.rounds[turns].nodesInRange.mapNotNull{(i, nodes)-> i.takeIf{nodes.isNotEmpty()}}.sorted()}")
+        println("WAIT")
+    }
+}
+
+fun findValidActions(simulator: GameSimulator): List<Int> {
+    val state = simulator.getFirstState().play(-1, simulator)
+
+    val toVisit = ArrayDeque<State>().apply { add(state) }
+    val visited = mutableMapOf(state.hash to state.bombCount)
+
+    var stateCount = 0
+    while (toVisit.isNotEmpty()) {
+        val current = toVisit.removeLast()
+        stateCount++
+        if (current.isWin()) {
+            System.err.println("states : $stateCount")
+            return current.actions
+        }
+        if (current.isFinal()) {
+            continue
+        }
+        for (action in current.getPossibleActions(simulator).reversed()) {
+            val child = current.play(action, simulator)
+            val  visitedBombs = visited[child.hash] ?: -1
+
+           if (child.bombCount > visitedBombs) {
+                toVisit.addLast(child)
+                visited[child.hash] = child.bombCount
+            }
+        }
+
+        if (stateCount % 100 == 0) {
+            System.err.println("states : $stateCount")
+        }
+
+    }
+    System.err.println(stateCount)
+    return emptyList()
 }
--- Puzzles/VoxCodei-Episode2/src/main/kotlin/fr/vco/codingame/puzzles/voxcodei/episode2/SimulatorBuilder.kt
+++ Puzzles/VoxCodei-Episode2/src/main/kotlin/fr/vco/codingame/puzzles/voxcodei/episode2/SimulatorBuilder.kt
@@ -1,5 +1,9 @@
 package fr.vco.codingame.puzzles.voxcodei.episode2
 
+import kotlin.system.measureTimeMillis
+
+const val NODE = '@'
+const val WALL = '#'
 
 val DIRECTIONS = listOf(
     Position(0, 0),
@@ -9,109 +13,157 @@ val DIRECTIONS = listOf(
     Position(-1, 0)
 )
 
-data class Node(val pos: Position, val direction: Position = Position(0, 0)) {
-    fun reverse() = Node(pos, -direction)
-    fun next() = Node(pos + direction, direction)
 
+data class Step(val pos: Position, val direction: Position = Position(0, 0)) {
+    fun reverse() = Step(pos, -direction)
+    fun next() = Step(pos + direction, direction)
 }
 
+data class Node(var trajectories: List<List<Step>>)
+
 class SimulatorBuilder(val width: Int, val height: Int) {
-    var possibleDirection: Map<Position, Set<Node>> = emptyMap()
-    fun updateGrid(grid: List<String>) {
-        val walls = mutableListOf<Position>()
-        val nodePositions = mutableListOf<Position>()
 
-        repeat(height) { y ->
-            repeat(width) { x ->
-                when (grid[y][x]) {
-                    '@' -> nodePositions.add(Position(x, y))
-                    '#' -> walls.add(Position(x, y))
-                }
-            }
+    val grid = MutableList(height) { "" }
+    lateinit var ranges: List<List<Int>>
+    lateinit var possibleMoves: List<List<Step>>
+    lateinit var possibleNodeDirections: List<Node>
+
+    private fun init() {
+        val (turns, _) = readln().split(" ").map { it.toInt() }
+        repeat(height) {
+            grid[it] = readln()
         }
-
-        if (possibleDirection.isEmpty()) {
-            possibleDirection = nodePositions.associateWith { node -> DIRECTIONS.map { Node(node, it) }.toSet() }
-        } else {
-            possibleDirection = possibleDirection.map { (node, dirs) ->
-                node to dirs.mapNotNull { nextNode(it, grid) }.filter { grid[it.pos.y][it.pos.x] == '@' }.toSet()
-            }.toMap()
-        }
+        measureTimeMillis {
+            ranges = computeRanges(grid)
+            possibleMoves = computePossibleMoves(grid)
+            possibleNodeDirections = initNodes(grid, turns)
+        }.let { System.err.println("Init Simulator Builder in ${it}ms") }
     }
 
-    private fun nextNode(node: Node, grid: List<String>): Node? {
-        var next = node.next()
-        if (next.pos.x in 0 until width &&
-            next.pos.y in 0 until height &&
-            grid[next.pos.y][next.pos.x] != '#'
-        ) {
-            return next
-        }
-        next = node.reverse().next()
-        if (next.pos.x in 0 until width &&
-            next.pos.y in 0 until height &&
-            grid[next.pos.y][next.pos.x] != '#'
-        ) {
-            return next
-        }
-        return null
-    }
-
-    fun isReady(): Boolean {
-        return possibleDirection.isNotEmpty() && possibleDirection.values.all { it.size == 1 }
-    }
-
-    private fun getNodes(): List<Node> {
-        return possibleDirection.values.map { it.first() }
-    }
-
-    fun buildSimulator(): GameSimulator {
-
-        while(!isReady()) {
-            val (rounds, bombs) = readln().split(" ").map { it.toInt() }
-            val grid = List(height) { readln() }
-            updateGrid(grid)
-            
-            if(isReady()) {
-                var currentNodes = getNodes()
-                val grids = List(rounds) {
-                    val currentGrid = buildGrid(currentNodes.map { it.pos }, grid)
-                    currentNodes = currentNodes.map { nextNode(it, grid)!! }
-                    currentGrid
-                }
-                return GameSimulator(width, height, bombs, rounds, currentNodes.size, grids.reversed(), buildRange(grid))
-            }
-            println("WAIT")
-        }
-        
-        throw Exception("unable to build Simulator")
-    }
-
-    private fun buildGrid(nodes: List<Position>, grid: List<String>): List<Int> {
-        val currentGrid = grid.flatMap { line ->
-            line.map { if (it == '#') WALL else EMPTY }.toMutableList()
-        }.toMutableList()
-        nodes.forEachIndexed { i, it -> currentGrid[it.y * width + it.x] = i }
-        return currentGrid
-    }
-
-    private fun buildRange(grid: List<String>): List<List<Int>> {
+    private fun computeRanges(grid: List<String>): List<List<Int>> {
+        val directions = DIRECTIONS.drop(1)
         return (0 until height).flatMap { y ->
             (0 until width).map { x ->
-                if (grid[y][x] == '#') {
+                if (grid[y][x] == WALL) {
                     emptyList()
                 } else {
                     val start = Position(x, y)
                     listOf(start.toIndex()) +
-                        DIRECTIONS.drop(1).flatMap { dir ->
-                            (1..BOMB_RANGE).map { dir * it + start }
-                                .takeWhile { it.x in 0 until width && it.y in 0 until height && grid[it.y][it.x] != '#' }
-                                .map{it.toIndex()}
-                        }
+                            directions.flatMap { dir ->
+                                (1..BOMB_RANGE).map { dir * it + start }
+                                    .takeWhile { it.x in 0 until width && it.y in 0 until height && grid[it.y][it.x] != WALL }
+                                    .map { it.toIndex() }
+                            }
                 }
             }
         }
     }
-    
+
+    private fun computePossibleMoves(grid: List<String>): List<List<Step>> {
+        val possibleMoves = List(height * width) { i ->
+            DIRECTIONS.map { dir -> nextStep(Step(i.toPosition(), dir), grid) }
+        }
+        return possibleMoves
+    }
+
+    private fun initNodes(grid: List<String>, turns: Int): List<Node> {
+        val nodes = mutableListOf<Node>()
+        repeat(height) { y ->
+            repeat(width) { x ->
+                if (grid[y][x] == NODE) {
+                    val nodePos = Position(x, y)
+                    val trajectories = DIRECTIONS.map { computeTrajectory(Step(nodePos, it), turns, grid) }
+                    val uniqueTrajectories = mutableListOf<List<Step>>()
+                    for (trajectory in trajectories) {
+                        if (trajectory.first() !in uniqueTrajectories.map { it.first() }) {
+                            uniqueTrajectories.add(trajectory)
+                        }
+                    }
+                    nodes.add(Node(uniqueTrajectories))
+                }
+            }
+        }
+
+        System.err.println(nodes.size)
+        return nodes
+    }
+
+    private fun computeTrajectory(start: Step, turns: Int, grid: List<String>): List<Step> {
+        val trajectory = mutableListOf(start)
+        for (turn in 1..turns) {
+            val last = trajectory[turn - 1]
+            trajectory.add(nextStep(last, grid))
+        }
+        return trajectory.reversed()
+    }
+
+    private fun updatePossibleNodeDirections(grid: List<String>, turn: Int) {
+        possibleNodeDirections.forEach { node ->
+            node.trajectories = node.trajectories.filter { grid[it[turn].pos] == NODE }
+        }
+    }
+
+    private fun nextStep(step: Step, grid: List<String>): Step {
+        var next = step.next()
+        if (next.pos.x in 0 until width &&
+            next.pos.y in 0 until height &&
+            grid[next.pos] != WALL
+        ) {
+            return next
+        }
+        next = step.reverse().next()
+        if (next.pos.x in 0 until width &&
+            next.pos.y in 0 until height &&
+            grid[next.pos] != WALL
+        ) {
+            return next
+        }
+        return Step(step.pos)
+    }
+
+    private fun isReady(): Boolean {
+        return possibleNodeDirections.all { it.trajectories.size == 1 }
+    }
+
+    fun buildSimulator(): GameSimulator {
+        init()
+        println("WAIT")
+
+        while (!isReady()) {
+            val (turns, bombs) = readln().split(" ").map { it.toInt() }
+            measureTimeMillis {
+                val grid = List(height) { readln() }
+                updatePossibleNodeDirections(grid, turns)
+            }.let { System.err.println("updateGrid in ${it}ms") }
+
+            if (isReady()) {
+                System.err.println("simulator ready")
+                val start = System.currentTimeMillis()
+
+                val rounds = List(turns + 1) { turn ->
+                    val nodes = possibleNodeDirections.map { node -> node.trajectories.first()[turn].pos.toIndex() }
+                    val nodesInRange = List(ranges.size) { mutableListOf<Int>() }
+                    nodes.forEachIndexed { nodeId, nodePos ->
+                        ranges[nodePos].forEach { pos ->
+                            nodesInRange[pos].add(nodeId)
+                        }
+                    }
+                    Round(
+                        nodes,
+                        nodesInRange.indices.associateWith { nodesInRange[it]  }.filter {(_, range) -> range.isNotEmpty()}
+                    )
+                }
+                System.err.println("Simulator initied in ${System.currentTimeMillis() - start}ms")
+
+                return GameSimulator(width, height, bombs, turns, possibleNodeDirections.size, ranges, rounds)
+            }
+            println("WAIT")
+        }
+        throw Exception("unable to build Simulator")
+    }
+
     private fun Position.toIndex() = y * width + x
+    private fun Int.toPosition() = Position(this % width, this / width)
+
+    private operator fun List<String>.get(pos: Position) = this.getOrNull(pos.y)?.getOrNull(pos.x) ?: WALL
 }
